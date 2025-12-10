109915f9504148b7c550d7d4809df87a81e9e073
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-12-10 07:56:19 +0100

    Fix opening brace spacing violations

--- Sources/RFC 2369/RFC_2369.List.Post.swift
+++ Sources/RFC 2369/RFC_2369.List.Post.swift
@@ -123,13 +123,11 @@ extension RFC_2369.List.Post: Binary.ASCII.Serializable {
 
         // Strip leading/trailing whitespace
         while !byteArray.isEmpty
-            && (byteArray.first == .ascii.space || byteArray.first == .ascii.htab)
-        {
+            && (byteArray.first == .ascii.space || byteArray.first == .ascii.htab) {
             byteArray.removeFirst()
         }
         while !byteArray.isEmpty
-            && (byteArray.last == .ascii.space || byteArray.last == .ascii.htab)
-        {
+            && (byteArray.last == .ascii.space || byteArray.last == .ascii.htab) {
             byteArray.removeLast()
         }
 
